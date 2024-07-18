a2e9f44094d73effdc6d4228be26c0ffd10c9236
Vinicius Rodrigues José <vinicius.rjose@outlook.com>
2024-07-18 23:21:42 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: 'uploads/' });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
